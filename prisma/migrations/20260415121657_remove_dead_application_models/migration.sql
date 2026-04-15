4ac8cf4e17e3faa0df91215fbf725f9d73af2290
Anastasios Andreou <anastasiosandreou1@gmail.com>
2026-04-15 15:22:06 +0300

    Changes

---
+++ prisma/migrations/20260415121657_remove_dead_application_models/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `applications` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `programs` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "public"."applications";
+
+-- DropTable
+DROP TABLE "public"."programs";
